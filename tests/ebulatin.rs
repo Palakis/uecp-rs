dfa93b159a97f1d50384453ab0d3cc2cc821e85f
Stéphane Lepin <stephane.lepin@gmail.com>
2019-10-23 10:42:27 +0200

    ebulatin(spec): fix array asserts

--- tests/ebulatin.rs
+++ tests/ebulatin.rs
@@ -6,10 +6,7 @@ fn test_to_e1_simple() {
     let result = uecp_rs::ebulatin::to_e1(source_str);
 
     let result_bytes = result.to_bytes();
-    assert_eq!(result_bytes[0], 0x74);
-    assert_eq!(result_bytes[1], 0x65);
-    assert_eq!(result_bytes[2], 0x73);
-    assert_eq!(result_bytes[3], 0x74);
+    assert_eq!(result_bytes, vec![0x74, 0x65, 0x73, 0x74]);
 }
 
 #[test]
@@ -18,12 +15,5 @@ fn test_to_e1_special_chars() {
     let result = uecp_rs::ebulatin::to_e1(source_str);
 
     let result_bytes = result.to_bytes();
-    assert_eq!(result_bytes[0], 0x82);
-    assert_eq!(result_bytes[1], 0x81);
-    assert_eq!(result_bytes[2], 0x9B);
-    assert_eq!(result_bytes[3], 0x99);
-    assert_eq!(result_bytes[4], 0x97);
-    assert_eq!(result_bytes[5], 0x23);
-    assert_eq!(result_bytes[6], 0xAB);
-    assert_eq!(result_bytes[7], 0xAA);
+    assert_eq!(result_bytes, vec![0x82, 0x81, 0x9B, 0x99, 0x97, 0x23, 0xAB, 0xAA]);
 }
