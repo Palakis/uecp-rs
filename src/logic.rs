1e63c9922d48db6422a3af32c5cb58f7c1987817
Stéphane Lepin <stephane.lepin@gmail.com>
2019-11-29 16:38:40 +0100

    logic: fixup

--- src/logic.rs
+++ src/logic.rs
@@ -5,10 +5,6 @@ use crate::protocol::{ Frame, MessageElement };
 pub fn process_incoming_frame<F>(request: &Frame, cb: F) -> Option<Frame>
     where F: Fn(&Frame) -> ResponseCode
 {
-    if request.elements.len() == 0 {
-        return None
-    }
-
     let response_code = cb(request);
 
     let mut ack_data: Vec<u8> = vec![response_code as u8];
